2f289f784f0fd359f194965254c98d8c6efae99c
ElizabethLankford <elizabethlankford1@gmail.com>
2024-01-12 15:29:05 -0600

    set up app home route

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 8081;
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const client = require("./db/client");
+client.connect();
+
+app.get("/", (req, res) => {
+  res.send("Home path WORKS!!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
