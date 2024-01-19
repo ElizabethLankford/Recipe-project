564952a70f577a1c010e2a292960bc1d665e7083
ElizabethLankford <elizabethlankford1@gmail.com>
2024-01-19 11:08:44 -0600

    set up working forms for login and register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { useLoginMutation } from "../redux/recipeApi";
+import { setToken } from "../redux/tokenSlice";
+
 function Login() {
-  return <div>Login page</div>;
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [login] = useLoginMutation();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    const { username, password } = user;
+    await login({ username, password })
+      .unwrap()
+      .then((response) => setToken(response.token))
+      .catch((rejected) => console.error(rejected));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label>
+          Username:
+          <input
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,82 @@
+import { useState } from "react";
+import { useRegisterMutation } from "../redux/recipeApi";
+import { setToken } from "../redux/tokenSlice";
+
 function Register() {
-  return <div>Register page</div>;
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [register] = useRegisterMutation();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    const { firstname, lastname, username, email, password, confirmPassword } =
+      user;
+    if (password === confirmPassword) {
+      console.log("passwords Match!");
+      await register({ username, password, firstname, lastname, email })
+        .unwrap()
+        .then((fulfilled) => setToken(fulfilled.token))
+        .catch((rejected) => console.error(rejected));
+    } else if (password !== confirmPassword) {
+      console.log("Error: passwords do not match!");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Register </h2>
+        <label>
+          Firstname:
+          <input
+            onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+          />
+        </label>
+        <label>
+          Lastname:
+          <input
+            onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+          />
+        </label>
+        <label>
+          Username:
+          <input
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </label>
+        <label>
+          Confirm password:
+          <input
+            type="password"
+            onChange={(e) =>
+              setUser({ ...user, confirmPassword: e.target.value })
+            }
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
