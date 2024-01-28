8072c3f17f37c528f540681ce1177e7cceb83db6
ElizabethLankford <elizabethlankford1@gmail.com>
2024-01-28 16:54:30 -0600

    update url sting

--- server/db/client.js
+++ server/db/client.js
@@ -1,5 +1,7 @@
 const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:54321/recipedatabase");
+const client = new Client(
+  "postgres://elankford:2vlyd8U950nmIvKCfvcnu9cJb5O10lvu@dpg-cmrdf1v109ks73fhj0v0-a/recipedatabase_pnu7"
+);
 
 module.exports = client;
