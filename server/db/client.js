7ae9062c0adebdf9095ac1eae439d46e6f56db7b
ElizabethLankford <elizabethlankford1@gmail.com>
2024-01-28 17:54:12 -0600

    updated client string

--- server/db/client.js
+++ server/db/client.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const client = new Client(
-  "postgres://elankford:2vlyd8U950nmIvKCfvcnu9cJb5O10lvu@dpg-cmrdf1v109ks73fhj0v0-a/recipedatabase_pnu7"
+  "postgres://recipedatabase_itx5_user:OEWbfILuWlCeCujk5mValm4QnU2U0e5m@dpg-cmreh3un7f5s738j8lm0-a/recipedatabase_itx5"
 );
 
 module.exports = client;
